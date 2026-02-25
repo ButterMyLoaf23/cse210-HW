01075782c10766bb0af2379154b3295f248d85cb
ButterMyLoaf23 <bostonwyatt1@gmail.com>
2026-02-25 12:54:42 -0700

    Stretch challenge done on prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,79 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+        Console.WriteLine();
+
+        if (number >= 97)
+        {
+            Console.WriteLine("Your grade is A+");
+            Console.WriteLine();
+        }
+        else if (number >= 94)
+        {
+            Console.WriteLine("Your grade is A");
+            Console.WriteLine();
+        }
+        else if (number >= 90)
+        {
+            Console.WriteLine("Your grade is A-");
+            Console.WriteLine();
+        }
+        else if (number >= 87)
+        {
+            Console.WriteLine("Your grade is B+");
+            Console.WriteLine();
+        }
+        else if (number >= 84)
+        {
+            Console.WriteLine("Your grade is B");
+            Console.WriteLine();
+        }
+        else if (number >= 80)
+        {
+            Console.WriteLine("Your grade is B-");
+            Console.WriteLine();
+        }
+        else if (number >= 77)
+        {
+            Console.WriteLine("Your grade is C+");
+            Console.WriteLine();
+        }
+        else if (number >= 74)
+        {
+            Console.WriteLine("Your grade is C");
+            Console.WriteLine();
+        }
+        else if (number >= 70)
+        {
+            Console.WriteLine("Your grade is C-");
+            Console.WriteLine();
+        }
+        else if (number >= 67)
+        {
+            Console.WriteLine("Your grade is D+");
+            Console.WriteLine();
+        }
+        else if (number >= 64)
+        {
+            Console.WriteLine("Your grade is D");
+            Console.WriteLine();
+        }
+        else if (number >= 60)
+        {
+            Console.WriteLine("Your grade is D-");
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("Your grade is F");
+            Console.WriteLine();
+        }
     }
 }
