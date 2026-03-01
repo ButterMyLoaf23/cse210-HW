4dc936d15a22dfa3d0a2c2b9589706b787323737
ButterMyLoaf23 <bostonwyatt1@gmail.com>
2026-02-28 20:45:30 -0700

    finished prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -6,21 +6,45 @@ class Program
     {
         List<int> numbers = new List<int>();
 
-        int number = -1;
+        int userNumber = -1;
+
+        // I added a do while loop here because I think it's better this way.
         do
         {
             Console.Write("Enter a number, enter 0 to end: ");
             string input = Console.ReadLine();
-            number = int.Parse(input);
+            userNumber = int.Parse(input);
 
-            do
+            if (userNumber != 0)
             {
-                numbers.Add(number);
+                numbers.Add(userNumber);
             }
-            while (number != 0);
 
         }
-        while (number != 0);
+        while (userNumber != 0);
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum of all the numbers is: {sum}");
+
+        float averageNum = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average of all numbers is: {averageNum}");
+
+        int highest = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > highest)
+            {
+                highest = number;
+            }
+        }
+
+        Console.WriteLine($"The highest number is: {highest}");
 
     }
 }
