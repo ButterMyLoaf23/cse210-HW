5830b92bd834bcd88b3090ed1607d31d7fd4891e
ButterMyLoaf23 <bostonwyatt1@gmail.com>
2026-03-03 21:16:43 -0700

    Half of my program is finished

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main()
     {
+        Job job1 = new Job();
+        job1.jobTitle = "Software Engineering";
+        job1.company = "Microsoft";
+        job1.startYear = 2019;
+        job1.endYear = 2022;
+
+        Job job2 = new Job();
+        job2.jobTitle = "Manager";
+        job2.company = "Apple";
+        job2.startYear = 2022;
+        job2.endYear = 2024;
+
         
     }
 }
