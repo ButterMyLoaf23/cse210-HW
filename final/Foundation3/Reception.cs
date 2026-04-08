86bfa6e0501bdce35dfa6730dff516f3c73f6a76
ButterMyLoaf23 <bostonwyatt1@gmail.com>
2026-04-07 18:53:27 -0700

    reception class file finished

---
+++ final/Foundation3/Reception.cs
@@ -0,0 +1,18 @@
+public class Reception : Event
+{
+    private string _rsvpEmail;
+    public Reception(string title, string description, string date, string time, Address address, string rsvpEmail) : base(title, description, date, time, address)
+    {
+        _rsvpEmail = rsvpEmail;
+    }
+
+    public override string GetFullDetails()
+    {
+        return GetStandardDetails() + $"\n Type: Reception \n RSVP: {_rsvpEmail}";
+    }
+
+    public override string GetShortDescription()
+    {
+        return $"Reception - {base.GetShortDescription()}";
+    }
+}
