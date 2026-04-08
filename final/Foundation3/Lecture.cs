8068c900bb453bdecd5c15fd8b593be47868ab9b
ButterMyLoaf23 <bostonwyatt1@gmail.com>
2026-04-07 19:12:37 -0700

    lecture class finished

---
+++ final/Foundation3/Lecture.cs
@@ -0,0 +1,22 @@
+public class Lecture : Event
+{
+    private string _speaker;
+    private int _capacity;
+
+    public Lecture(string title, string description, string date, string time, Address address, string speaker, int capacity) : base(title, description, date, time, address)
+    {
+        _speaker = speaker;
+        _capacity = capacity;   
+    }
+
+    public override string GetFullDetails()
+    {
+        return GetStandardDetails() + $"\n Type: Lecture \n Speaker: {_speaker} \n Capacity: {_capacity}";
+    }
+
+    public override string GetShortDescription()
+    {
+        return $"Lecture: {_speaker} - {base.GetShortDescription()}";
+    }
+
+}
